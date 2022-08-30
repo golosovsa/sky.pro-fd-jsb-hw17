f532752afe70c75afd96ab82469dc2fa3f49d4cc
grm <golosov_SA@mail.ru>
2022-08-30 09:11:28 +0300

    fix bugs

--- jsb-17-1/js/main.js
+++ jsb-17-1/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    require.config({ paths: { vs: '/jsb-17-1/node_modules/monaco-editor/min/vs' } });
+    require.config({ paths: { vs: 'node_modules/monaco-editor/min/vs' } });
 
     const editorContainer = document.querySelector(".editor__container");
 
